42e156dd572b6daf531ab1f06e7bff959d32ab91
Enrique Alba <enalsa2222@gmail.com>
2025-08-24 19:39:08 +0200

    mock login action

--- src/pages/auth/login-page.spec.tsx
+++ src/pages/auth/login-page.spec.tsx
@@ -1,7 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import { userEvent } from "@testing-library/user-event";
+import userEvent from "@testing-library/user-event";
 import LoginPage from "./login-page";
 import { Provider } from "react-redux";
+import * as hooks from "../../store/hooks";
 
 describe("LoginPage", () => {
   const state = {
@@ -24,22 +25,40 @@ describe("LoginPage", () => {
       </Provider>,
     );
 
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
   test("should render", () => {
     const { container } = renderComponent();
     expect(container).toMatchSnapshot();
   });
 
-  test("Should dispatch login action", async () => {
+  test("Should dispatch login action with form credentials", async () => {
+    const mockLoginAction = vi.fn().mockResolvedValue(undefined);
+    vi.spyOn(hooks, "useLoginAction").mockReturnValue(mockLoginAction);
+
     renderComponent();
+
     const emailInput = screen.getByLabelText("Email");
     const passwordInput = screen.getByLabelText("Password");
     const button = screen.getByRole("button");
     expect(button).toHaveTextContent("Log in!");
+
     expect(button).toBeDisabled();
 
     await userEvent.type(emailInput, "user@mail.com");
     await userEvent.type(passwordInput, "1234");
+
     expect(button).toBeEnabled();
+
     await userEvent.click(button);
+
+    expect(mockLoginAction).toHaveBeenCalledTimes(1);
+    expect(mockLoginAction).toHaveBeenCalledWith({
+      email: "user@mail.com",
+      password: "1234",
+      remember: false,
+    });
   });
 });
